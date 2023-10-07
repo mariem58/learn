3d631585b9c524d62ae21f06f1e1ca10211b1bf6
mariem58 <100387600+mariem58@users.noreply.github.com>
2023-10-07 17:12:24 +0300

    Add Class Demo

---
+++ Assos1/src/learn/Demo.java
@@ -0,0 +1,12 @@
+package learn;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello learn");
+		System.out.print("Welcome");
+
+	}
+
+}
