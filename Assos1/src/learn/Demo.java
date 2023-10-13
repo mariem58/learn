549acb8a24382cc7fd0583b24e629a2680080c20
mariem58 <100387600+mariem58@users.noreply.github.com>
2023-10-13 23:11:53 +0300

    Person Parent Class

--- Assos1/src/learn/Demo.java
+++ Assos1/src/learn/Demo.java
@@ -5,11 +5,24 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello learn");
-		 
+		System.out.print("\n");
 		Car bmw=new Car();
 		bmw.setColor("Red");
-		System.out.print("\n");
 		System.out.print(bmw.getColor());
+		System.out.print("\n");
+		System.out.print("\n");
+		
+		
+		
+		Boy b1=new Boy();
+		b1.setAge(28);
+		System.out.print(b1.getAge());
+		
+		System.out.print("\n");
+		
+		Girl g1=new Girl();
+		g1.setName("Norhan");
+		System.out.print(g1.getName());
 		}
 
 }
---
+++ Assos1/src/learn/Person.java
@@ -0,0 +1,34 @@
+package learn;
+
+public class Person {
+	private String name;
+	private int age;
+	private double height;
+	private int grade;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+
+}
